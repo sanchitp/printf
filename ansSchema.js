1e1a7bf613af7d8b1f1d2a338b01e24866fac5b7
anjali22 <anjaliprajapati3122@gmail.com>
2014-03-04 21:38:31 +0530

    Create ansSchema.js

---
+++ ansSchema.js
@@ -0,0 +1,34 @@
+var redis =require("redis");
+var client =redis.createClient();
+client.on("error",function(err) {
+console.log("error is " + err);
+});
+
+
+module.exports.setAnswers=function(ansno,ans,callback) {
+
+var key=ans;
+var index=parseInt('ansno');
+client.lset(key,index,ans,function (err,data){
+									if(!err){
+										
+										console.log("successful!!");
+										callback(null,data);
+	
+									}else{
+										callback(err,null);
+									}
+								});
+}
+
+module.exports.getAnswers=function(key,callback) {
+
+client.lrange(key,0,-1,function(err,data){
+	if(!err){
+		console.log("successful");
+		callback(null,data);
+
+	}
+	else{ callback(err,null);}
+});
+}
