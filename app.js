d8734671c98277232bbbd91b5a6cfd417c827a9f
laukik <laukiknirgudkar@gmail.com>
2014-03-04 15:30:22 +0545

    with structure

--- app.js
+++ app.js
@@ -4,17 +4,12 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
-
+var db = require("redis").createClient();
 var app = express();
-var Question=require('./schema');
-
-
-
-
+var RedisStore = require('connect-redis')(express);
+var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -25,48 +20,44 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session({
+	store: new RedisStore({
+    host: 'localhost',
+    port: 6379,
+    db: 2
+  }),
+  secret: '1234567890QWERTY'
+}));
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-var que=[];
-var opt=[];
-var k=1;
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
+
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-
-app.get('/',function(req,res){
-
-	var Qid;
-var data;
-
-
-
-	for(i=0;i<3;i++){
-		!function syn(i){
-			Qid=i+1;
-			Question.getQuestion(Qid, function (err, question_data){
-				if( !err ){
-					opt.push(question_data[1]);
-					opt.push(question_data[2]);
-					opt.push(question_data[3]);
-					opt.push(question_data[4]);
-					que.push(question_data[0]);
-					if(i==2){
-						res.render('ques',{title: 'printf()',question:que, option:opt});
-					}
-				}	
-				else{
-						console.log('inside /retrive while getQuestion');
-					}
-			});
-		}(i);
-	}
-});
-
-
+require('./controllers/master')(app, db);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+
+//socket.io
+
+var io = require('socket.io').listen(server);  
+io.enable('browser client minification'); // send minified client
+io.enable('browser client etag'); // apply etag caching logic based on version number
+io.enable('browser client gzip'); // gzip the file
+io.set('log level', 1);
+io.set('browser client expires',315360000); // reduce logging
+io.set('transports', [ // enable all transports (optional if you want flashsocket)
+    'websocket'
+  , 'flashsocket'
+  , 'htmlfile'
+  , 'xhr-polling'
+  , 'jsonp-polling'
+]);
+
+require('./socket/socket_methods')(io,db);
