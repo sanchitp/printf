4f46b390bffce1b91a75fd9c07a83c16b035a893
laukik <laukiknirgudkar@gmail.com>
2014-03-04 20:53:39 +0530

    added check password

--- models/user.js
+++ models/user.js
@@ -3,6 +3,27 @@
  * GET users listing.
  */
 
-exports.list = function(req, res){
-  res.send("respond with a resource");
+ var user_schema = {
+ 	set_name : "users" 
+ };
+
+
+/* to validate user password */
+module.exports.check_password = function( db, user_name, user_password, callback){
+	db.hget( user_schema.set_name, user_name, function ( err, password ){
+		if( !err ){
+			if( !password ){
+				/*  user does not exists */
+				callback( null, 0);
+			}else{
+				if( password == user_password){
+					callback( null, 1 );
+				}else{
+					callback( null, 2);
+				}
+			}
+		}else{
+			consol.log('ERR TO FETCH DATA AT check_password');
+		}
+	});
 };
